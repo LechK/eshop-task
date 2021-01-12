136792f6abdc4c25a80f3ed146ddac20b76a506f
LechKuznecov <46899047+LechKuznecov@users.noreply.github.com>
2021-01-12 19:12:16 +0200

    global-styles added

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import Routes from "./Routes";
 import { ThemeProvider } from "styled-components";
 import theme from "./theme";
+import { GlobalStyles } from "./global";
 import "normalize.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Routes />
     </ThemeProvider>
   </React.StrictMode>,
