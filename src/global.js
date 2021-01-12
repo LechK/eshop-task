c519d43fa40bf30d344f27616b9e25917533f21e
LechKuznecov <46899047+LechKuznecov@users.noreply.github.com>
2021-01-13 00:40:44 +0200

    Open sans font added

--- src/global.js
+++ src/global.js
@@ -5,6 +5,6 @@ export const GlobalStyles = createGlobalStyle`
     background: ${({ theme }) => theme.light};
     color: ${({ theme }) => theme.dark};    
     text-rendering: optimizeLegibility;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: 'Open Sans', sans-serif;
   }
 `;
