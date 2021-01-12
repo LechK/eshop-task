9f9c0c83caaed93ae757d7d7ef77d455a5b122b6
LechKuznecov <46899047+LechKuznecov@users.noreply.github.com>
2021-01-12 19:11:37 +0200

    init

---
+++ src/theme.js
@@ -0,0 +1,24 @@
+const theme = {
+  white: "rgb(255, 255, 255)",
+  light: "rgb(248, 244, 241)",
+  dark: "rgb(41, 39, 43)",
+
+  primary: {
+    background: "#222222",
+    color: "#ffffff",
+    hover: {
+      background: "rgb(216,221,222)",
+      color: "#eeeeee",
+    },
+  },
+  secondary: {
+    background: "#333333",
+    color: "purple",
+    hover: {
+      background: "green",
+      color: "cyan",
+    },
+  },
+};
+
+export default theme;
