b0981404d7f3f1a9e659711284fee78198a46a8e
LechKuznecov <46899047+LechKuznecov@users.noreply.github.com>
2021-01-15 00:18:32 +0200

    array of products added

---
+++ src/utils/products.js
@@ -0,0 +1,88 @@
+const products = [
+  {
+    id: 1,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=38e923f6-5fdd-4d1b-885b-35a43ea5166d",
+    name: "Virta ir rūkyta kaimiško dūmo skonio KRIVIO šoninė, 1 kg",
+    price: 1249,
+  },
+  {
+    id: 2,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=6b46f18d-f014-456c-aef6-551f154eebdc",
+    name: "Sauskelnės MOONY NATURAL S (4-8 kg), 58 vnt.",
+    price: 2899,
+  },
+  {
+    id: 3,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=96d2ef63-e846-4fa6-9f25-268720a237ac",
+    name: "DVARO kefyras, 2,5% rieb., 900 g",
+    price: 93,
+  },
+  {
+    id: 4,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=938dd549-09b3-43bd-975f-8aae1405bca6",
+    name: "Ekologiška mėlynių ir obuolių tyrelė HIPP (nuo 4 mėn.), 125 g",
+    price: 129,
+  },
+  {
+    id: 5,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=68e894cc-aefc-4cd8-8bde-6735c9d8f021",
+    name: "Pieniška košė BIO HIP su obuol.ir pankoliais (LABOS NAKTIES), 250 g",
+    price: 339,
+  },
+  {
+    id: 6,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=be70010d-67cd-4e47-a9b7-48ef82da28a4",
+    name: "Namų kvapiklis LAUMĖS KERAI su lazdelėmis, 1 vnt.",
+    price: 1569,
+  },
+  {
+    id: 7,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=c923a1b1-b32e-45a9-b029-e2abe24e6214",
+    name: "Šunų pašaro papild. NATURE LIVING (galv.odos pagal.su višt.), 75 g",
+    price: 174,
+  },
+  {
+    id: 8,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=c5260700-4ad3-4d68-b8fa-a4cbea2becae",
+    name: "Šunų kramtalas NATURE LIVING (galvijų odos kaulas, 13 cm), 65 g",
+    price: 55,
+  },
+  {
+    id: 9,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=b31c1605-287a-4031-97ba-a20ddf8eaa73",
+    name: "Kačių skanėstas FELIX MIXED GRILL, 60 g",
+    price: 104,
+  },
+  {
+    id: 10,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=c00b9c2d-674b-44db-bfe1-7d8d7c826341",
+    name: "Sausas kačių ėdalas FAVORIT su jaut., višt. ir darž., 1 kg",
+    price: 139,
+  },
+  {
+    id: 11,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=7dd74146-9f1c-4411-badc-668c7a6b94e5",
+    name: "Malta kava LAVAZZA MOUNTAIN GROWN metalinėje dėžutėje, 250 g.",
+    price: 699,
+  },
+  {
+    id: 12,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=e8f1576c-dce0-40fb-a1b0-44d7c37338db",
+    name: "Barškutis CANPOL, 1 vnt.",
+    price: 405,
+  },
+];
+
+export default products;
